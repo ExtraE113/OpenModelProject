93e1614bce16de7c213a5f30b4ac3db29c230755
ExtraE113 <ezra@mamans.com>
2020-12-28 14:35:08 -0800

    update model

--- assets/js/model-switcher.js
+++ assets/js/model-switcher.js
@@ -7,22 +7,22 @@ function handleChange() {
     let scenario = (covid ? "c" : "x") + (avg.includes("imple") ? "s" : "w") + (market ? "p" : "x") + (prior ? "e" : "x")
 
     let data = {
-        "xsxx": [0.25, 0.29, "Margin: -1.9 (80% CI: -6 to 2.2)", 0.42, "Margin: -0.7 (80% CI: -4.8 to 3.4)"],
-        "csxx": [0.19, 0.22, "Margin: -2.6 (80% CI: -6.7 to 1.4)", 0.34, "Margin: -1.4 (80% CI: -5.5 to 2.6)"],
-        "xwxx": [0.27, 0.32, "Margin: -1.6 (80% CI: -5.7 to 2.5)", 0.41, "Margin: -0.7 (80% CI: -4.8 to 3.4)"],
-        "cwxx": [0.2, 0.24, "Margin: -2.5 (80% CI: -6.6 to 1.6)", 0.32, "Margin: -1.6 (80% CI: -5.7 to 2.5)"],
-        "xspx": [0.26, 0.3, "Margin: -1.9 (80% CI: -6 to 2.2)", 0.42, "Margin: -0.7 (80% CI: -4.8 to 3.4)"],
-        "cspx": [0.21, 0.24, "Margin: -2.6 (80% CI: -6.7 to 1.4)", 0.34, "Margin: -1.4 (80% CI: -5.5 to 2.6)"],
-        "xwpx": [0.28, 0.33, "Margin: -1.6 (80% CI: -5.7 to 2.5)", 0.41, "Margin: -0.7 (80% CI: -4.8 to 3.4)"],
-        "cwpx": [0.22, 0.25, "Margin: -2.5 (80% CI: -6.6 to 1.6)", 0.32, "Margin: -1.6 (80% CI: -5.7 to 2.5)"],
-        "xsxe": [0.25, 0.29, "Margin: -1.8 (80% CI: -5.9 to 2.2)", 0.41, "Margin: -0.8 (80% CI: -4.9 to 3.3)"],
-        "csxe": [0.22, 0.26, "Margin: -2.2 (80% CI: -6.3 to 1.9)", 0.36, "Margin: -1.2 (80% CI: -5.2 to 2.9)"],
-        "xwxe": [0.26, 0.31, "Margin: -1.7 (80% CI: -5.8 to 2.4)", 0.4, "Margin: -0.8 (80% CI: -4.9 to 3.3)"],
-        "cwxe": [0.22, 0.27, "Margin: -2.1 (80% CI: -6.2 to 2)", 0.35, "Margin: -1.3 (80% CI: -5.4 to 2.8)"],
-        "xspe": [0.26, 0.3, "Margin: -1.8 (80% CI: -5.9 to 2.2)", 0.4, "Margin: -0.8 (80% CI: -4.9 to 3.3)"],
-        "cspe": [0.24, 0.27, "Margin: -2.2 (80% CI: -6.3 to 1.9)", 0.37, "Margin: -1.2 (80% CI: -5.2 to 2.9)"],
-        "xwpe": [0.27, 0.32, "Margin: -1.7 (80% CI: -5.8 to 2.4)", 0.4, "Margin: -0.8 (80% CI: -4.9 to 3.3)"],
-        "cwpe": [0.24, 0.28, "Margin: -2.1 (80% CI: -6.2 to 2)", 0.36, "Margin: -1.3 (80% CI: -5.4 to 2.8)"]
+        "xsxx": [0.26, 0.31, "Margin: -1.7 (80% CI: -5.7 to 2.3)", 0.39, "Margin: -0.9 (80% CI: -5 to 3.1)"],
+        "csxx": [0.19, 0.23, "Margin: -2.6 (80% CI: -6.6 to 1.4)", 0.29, "Margin: -1.8 (80% CI: -5.8 to 2.2)"],
+        "xwxx": [0.36, 0.43, "Margin: -0.6 (80% CI: -4.7 to 3.4)", 0.43, "Margin: -0.6 (80% CI: -4.7 to 3.5)"],
+        "cwxx": [0.25, 0.31, "Margin: -1.7 (80% CI: -5.7 to 2.4)", 0.31, "Margin: -1.7 (80% CI: -5.7 to 2.4)"],
+        "xspx": [0.27, 0.31, "Margin: -1.7 (80% CI: -5.7 to 2.3)", 0.39, "Margin: -0.9 (80% CI: -5 to 3.1)"],
+        "cspx": [0.21, 0.24, "Margin: -2.6 (80% CI: -6.6 to 1.4)", 0.3, "Margin: -1.8 (80% CI: -5.8 to 2.2)"],
+        "xwpx": [0.35, 0.42, "Margin: -0.6 (80% CI: -4.7 to 3.4)", 0.42, "Margin: -0.6 (80% CI: -4.7 to 3.5)"],
+        "cwpx": [0.26, 0.32, "Margin: -1.7 (80% CI: -5.7 to 2.4)", 0.32, "Margin: -1.7 (80% CI: -5.7 to 2.4)"],
+        "xsxe": [0.26, 0.3, "Margin: -1.7 (80% CI: -5.8 to 2.3)", 0.39, "Margin: -0.9 (80% CI: -4.9 to 3.1)"],
+        "csxe": [0.22, 0.26, "Margin: -2.2 (80% CI: -6.2 to 1.8)", 0.34, "Margin: -1.4 (80% CI: -5.4 to 2.7)"],
+        "xwxe": [0.31, 0.36, "Margin: -1.2 (80% CI: -5.3 to 2.8)", 0.41, "Margin: -0.8 (80% CI: -4.8 to 3.3)"],
+        "cwxe": [0.25, 0.3, "Margin: -1.7 (80% CI: -5.8 to 2.3)", 0.35, "Margin: -1.3 (80% CI: -5.3 to 2.8)"],
+        "xspe": [0.27, 0.31, "Margin: -1.7 (80% CI: -5.8 to 2.3)", 0.39, "Margin: -0.9 (80% CI: -4.9 to 3.1)"],
+        "cspe": [0.23, 0.27, "Margin: -2.2 (80% CI: -6.2 to 1.8)", 0.35, "Margin: -1.4 (80% CI: -5.4 to 2.7)"],
+        "xwpe": [0.31, 0.36, "Margin: -1.2 (80% CI: -5.3 to 2.8)", 0.41, "Margin: -0.8 (80% CI: -4.8 to 3.3)"],
+        "cwpe": [0.26, 0.31, "Margin: -1.7 (80% CI: -5.8 to 2.3)", 0.35, "Margin: -1.3 (80% CI: -5.3 to 2.8)"]
     }
 
     let [dsenate, ossoff, ossoff_margin, warnock, warnock_margin] = data[scenario];
